d62c5a9c2a392864e6b94d50eadba32f72b5e0b7
rconaway-uno <41027252+rconaway-uno@users.noreply.github.com>
2019-02-06 15:11:07 -0600

    Initial commit

---
+++ crm/urls.py
@@ -0,0 +1,35 @@
+from django.conf.urls import url,include
+from . import views
+from django.urls import path, re_path
+from django.contrib.auth import views as auth_views
+
+
+app_name = 'crm'
+urlpatterns = [
+    path('', views.home, name='home'),
+    re_path(r'^home/$', views.home, name='home'),
+    path('customer_list', views.customer_list, name='customer_list'),
+    path('customer/<int:pk>/edit/', views.customer_edit, name='customer_edit'),
+    path('customer/<int:pk>/delete/', views.customer_delete, name='customer_delete'),
+    path('customer/create/', views.customer_new, name='customer_new'),
+    path('customer/<int:pk>/summary/', views.summary, name='summary'),
+    path('service_list', views.service_list, name='service_list'),
+    path('service/<int:pk>/edit/', views.service_edit, name='service_edit'),
+    path('service/<int:pk>/delete/', views.service_delete, name='service_delete'),
+    path('service/create/', views.service_new, name='service_new'),
+    path('product_list', views.product_list, name='product_list'),
+    path('product/<int:pk>/edit/', views.product_edit, name='product_edit'),
+    path('product/create/', views.product_new, name='product_new'),		
+    path('product/<int:pk>/delete/', views.product_delete, name='product_delete'),
+
+    #PW Reset
+
+    url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+    url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
+
+    #url('^', include('django.contrib.auth.urls')),
+
+]
